1c290c4483b1beb9fc857bc0327d4184e8e36efa
defnotnavs <navijonas28@gmail.com>
2025-08-04 11:58:57 +0800

    Update script.js

--- 05-function-factory/script.js
+++ 05-function-factory/script.js
@@ -1,17 +1,26 @@
-// Starter examples (function output expectations are provided in README)
+// Function to create a multiplier
+const makeMultiplier = (factor) => {
+    return (num) => num * factor;
+};
 
-const makeMultiplier = (factor) => (num) => num * factor;
+// Function to create a greeter
+const makeGreeter = (greeting) => {
+    return (name) => `${greeting}, ${name}!`;
+};
 
+// Output function to append messages to the body
+const appendToBody = (message) => {
+    const outputDiv = document.createElement('div');
+    outputDiv.innerHTML = message;
+    document.body.appendChild(outputDiv);
+};
+
+// Displaying the results of makeMultiplier
+appendToBody("<strong>Multiplier Function:</strong>");
 const double = makeMultiplier(2);
-console.log(double(5)); 
-const triple = makeMultiplier(3);
-console.log(triple(4)); 
+appendToBody(`Double of 5: ${double(5)}`); // 10
 
-console.log("------------------------------------------------------");
-
-const makeGreeter = (greeting) => (name) => `${greeting}, ${name}!`;
-
+// Displaying the results of makeGreeter
+appendToBody("<strong>Greeter Function:</strong>");
 const sayHi = makeGreeter("Hi");
-console.log(sayHi("Mia"));   
-const sayHello = makeGreeter("Hello");
-console.log(sayHello("Fred")); 
+appendToBody(sayHi("Mia")); // "Hi, Mia!"
